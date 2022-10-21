f94cf8efa23c01e5f792c3024997f260db0afbeb
Nelson20000 <nelsonoriongo@gmail.com>
2022-10-21 06:51:12 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,15 +15,17 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			if (c < 10)
-				b = c;
-			else
-			{
-				a = c / 10; b = c % 10;
-				_putchar('0' + a);
-			}
-			_putchar('0' + b);
+			if (c < 10);
+			b = c;
 		}
-		_putchar('\n');
+
+	else
+	{
+		a = c / 10; b = c % 10;
+		_putchar('0' + a);
 	}
+	_putchar('0' + b);
+	}	
+	_putchar('\n');
+
 }
